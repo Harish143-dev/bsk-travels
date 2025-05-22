8d07b6d4351ca280f1cfebe72d55a039addde5fd
Harish143-dev <saravananharish143@gmail.com>
2025-05-22 10:12:41 +0530

    first commit

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -23,20 +23,20 @@ const Login = () => {
                         <input type='password' className='inp-field px-2 py-1' id='pass' />
                     </div>
                     <div className='text-end'>
-                        <a href='#' className='need-help'>Need Help ?</a>
+                        <a href=' ' className='need-help'>Need Help ?</a>
                     </div>
                     <div className='text-center p-2'>
                         {<button className={`border-0 rounded-3 px-3 py-1 btn-log ${login === 'Sign In' ? 'btn-red' : 'btn-blue'}`}>
-                            <a className='text-decoration-none btn-login-link'>{login}</a>
+                            <a href=' ' className='text-decoration-none btn-login-link'>{login}</a>
                         </button>}
                     </div>
                 </div>
                 <div className='mt-3'>
                     {login === 'Sign In' ? <span className='other-opt'>
-                        Create a new Account <a href='#' className='other-opt-link-up' onClick={() => setLogin('Sign Up')}>
+                        Create a new Account <a href=' ' className='other-opt-link-up' onClick={() => setLogin('Sign Up')}>
                             Sign Up</a></span> : <></>}
                     {login === 'Sign Up' ? <p className='other-opt'>
-                        Already have a Account <a href='#' className='other-opt-link-in' onClick={() => setLogin('Sign In')}>
+                        Already have a Account <a href=' ' className='other-opt-link-in' onClick={() => setLogin('Sign In')}>
                             Sign In</a></p> : <></>}
                 </div>
             </div>
